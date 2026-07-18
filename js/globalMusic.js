a75c6f512ccc0d8c805a2af179c46fad7a599b17
illiasuissakah18 <illiasuissaka18@gmail.com>
2026-07-18 15:36:57 -0700

    Fixed continuous background music

--- js/globalMusic.js
+++ js/globalMusic.js
@@ -1,42 +1,85 @@
-const music = document.createElement("audio");
+// =======================================
+// GLOBAL BIRTHDAY MUSIC SYSTEM
+// =======================================
 
-music.src = "music/birthday.mp3";
+const music = new Audio();
+
+music.src = "music/background.mp3";
+
 music.loop = true;
+
 music.volume = 0.4;
 
 
-// Restore previous time
-const savedTime = localStorage.getItem("musicTime");
 
-if(savedTime){
-    music.currentTime = savedTime;
-}
-
-
-// Auto play
-window.addEventListener("load",()=>{
-
-    music.play()
-    .catch(()=>{
-
-        document.addEventListener("click",()=>{
-
-            music.play();
-
-        }, {once:true});
-
-    });
-
-});
-
-
-// Save position before leaving page
-
-setInterval(()=>{
+// Save music position
+function saveMusicPosition(){
 
     localStorage.setItem(
         "musicTime",
         music.currentTime
     );
+
+}
+
+
+
+// Restore previous position
+
+const savedTime =
+localStorage.getItem("musicTime");
+
+
+if(savedTime){
+
+    music.currentTime =
+    Number(savedTime);
+
+}
+
+
+
+// Try automatic play
+
+window.addEventListener("load",()=>{
+
+
+    music.play()
+    .then(()=>{
+
+        console.log("Music started");
+
+    })
+    .catch(()=>{
+
+
+        console.log(
+        "Autoplay blocked - waiting for user click"
+        );
+
+
+        document.addEventListener(
+        "click",
+        ()=>{
+
+            music.play();
+
+        },
+        {once:true}
+        );
+
+
+    });
+
+
+});
+
+
+
+// Save every second
+
+setInterval(()=>{
+
+    saveMusicPosition();
 
 },1000);
